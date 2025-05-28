8ab022af83de3c0681064ea40d46f0de9fd781e5
MernArtisan <mernartisan@gmail.com>
2025-05-29 03:45:31 +0500

    code

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::get('/run-migrate', function () {
+    if (request('key') !== 'secret-key') abort(403);
+    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+    return 'Migrations done!';
+});
 
 Route::get('/', function () { 
     return view('welcome'); 
