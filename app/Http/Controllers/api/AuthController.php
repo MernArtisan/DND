e1161192a15fdca82ede0644b40bde10bdc51299
MernArtisan <mernartisan@gmail.com>
2025-05-29 04:45:48 +0500

    s

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
-use app\Services\TwilioService;
+use App\Services\TwilioService;
 
 class AuthController extends Controller
 {
