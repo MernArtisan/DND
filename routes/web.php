cde754afe3e6c8383f3b59a5a639b7eda8ad5edf
MernArtisan <mernartisan@gmail.com>
2025-06-03 00:46:08 +0500

    etyertyre

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,21 @@ Route::get('/', function () {
     // return "abc";
 });
 
-// routes/api.php
 Route::get('/stream-info', function () {
     return response()->json([
         'appID' => env('ZEGO_APP_ID'),
         'appSign' => env('ZEGO_APP_SIGN'),
         'userID' => 'user_' . rand(1000, 9999),
-        'userName' => 'User ' . rand(1, 100),
+        'userName' => 'User_' . rand(1, 100),
         'roomID' => 'stream_002',
     ]);
 });
+
+Route::get('/stream', function () {
+    return response()->file(public_path('stream.html'));
+});
+
+
 Route::get('/zego-check', function () {
     if (env('ZEGO_APP_ID') && env('ZEGO_APP_SIGN')) {
         return response()->json(['status' => 'ok', 'message' => 'Zego credentials loaded']);
@@ -31,9 +36,4 @@ Route::get('/zego-check', function () {
         return response()->json(['status' => 'error', 'message' => 'Zego credentials missing']);
     }
 });
-
-// web.php
-Route::get('/stream', function () {
-    return response()->file(public_path('stream.html'));
-});
-
+ 
