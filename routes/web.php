5b74e22f3e249398d08736ac2917b96e56749561
MernArtisan <mernartisan@gmail.com>
2025-06-03 03:34:14 +0500

    sawe

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MigrationController;
-
-
+use Illuminate\Support\Facades\Response;
 
 Route::get('/migrate', [MigrationController::class, 'runAll']);
 Route::get('/migrate-1', [MigrationController::class, 'runSpecific']);
