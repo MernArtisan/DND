dab2ba071b33beb543e1681e58dd498000c14c95
MernArtisan <mernartisan@gmail.com>
2025-06-02 22:01:57 +0500

    as

--- app/Models/Channel.php
+++ app/Models/Channel.php
@@ -11,4 +11,15 @@ class Channel extends Model
     {
         return $this->belongsTo(User::class, 'streamer_id');
     }
+    public function getStreamerAttribute()
+    {
+        if ($this->banner && file_exists(public_path($this->banner) || file_exists(public_path($this->logo)))) {
+            return asset($this->banner ?? $this->logo);
+        }
+
+        if ($this->banner || $this->logo) {
+            return asset('default-woman.png');
+        }
+        return asset('default-man.png');
+    }
 }
