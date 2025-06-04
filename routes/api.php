4189ad97eeb5b125f4303c4a590f9880b777f27a
MernArtisan <mernartisan@gmail.com>
2025-06-04 05:16:11 +0500

    hello

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+
 // Authenticate apis
 Route::post('signup', [App\Http\Controllers\api\AuthController::class, 'signup']);
 Route::post('signin', [App\Http\Controllers\api\AuthController::class, 'signin']);
@@ -14,4 +17,32 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('profile', [App\Http\Controllers\api\ProfileController::class, 'profile']);
     // Channel Routes
     Route::post('channel/create', [App\Http\Controllers\api\ChannelController::class, 'create']);
+});
+
+Route::get('zego-token', function (Request $request) {
+    $appID = intval(env('ZEGO_APP_ID')); // ✅ 1269365093
+    $serverSecret = env('ZEGO_SERVER_SECRET'); // ✅ 5eb0af489d7d12bce51c1746097e667d
+
+    $userID = $request->query('userID'); // ✅ GET se userID lo
+    $roomID = $request->query('liveID'); // ✅ GET se roomID lo
+
+    if (!$userID || !$roomID) {
+        return response()->json(['error' => 'Missing userID or roomID'], 400);
+    }
+
+    $expire = time() + 3600;
+    $nonce = random_int(100000, 999999);
+
+    $payload = json_encode([
+        'app_id' => $appID,
+        'user_id' => $userID,
+        'room_id' => $roomID,
+        'nonce' => $nonce,
+        'expired' => $expire,
+    ]);
+
+    $signature = hash_hmac('sha256', $payload, $serverSecret, true);
+    $token = base64_encode($signature) . '.' . base64_encode($payload);
+
+    return response()->json(['token' => $token]);
 });
