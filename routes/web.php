d6a1df26fcd5e698135ab7f16817899f8481a894
MernArtisan <mernartisan@gmail.com>
2025-06-04 23:18:46 +0500

    dummy stream

--- routes/web.php
+++ routes/web.php
@@ -12,31 +12,3 @@ Route::get('/viewer', function () {
     return response()->file(public_path('stream.html'));
 });
 
-
-
-// routes/web.php
-Route::get('/zego-token', function () {
-    $appID = 1173379578; // your app ID
-    $serverSecret = '7094b1463e589f6c10d756d00a89648263426a00d725a02aadfb89dc553dc458'; // from Zego dashboard
-    $userID = 333;
-
-    if (!$userID) {
-        return response()->json(['error' => 'Missing userID'], 400);
-    }
-
-    $expire = time() + 3600;
-    $nonce = random_int(100000, 999999);
-
-    $payloadArray = [
-        'app_id' => $appID,
-        'user_id' => $userID,
-        'nonce' => $nonce,
-        'expired' => $expire,
-    ];
-
-    $payload = json_encode($payloadArray);
-    $signature = hash_hmac('sha256', $payload, $serverSecret, true);
-    $token = base64_encode($signature) . '.' . base64_encode($payload);
-
-    return response()->json(['token' => $token]);
-});
