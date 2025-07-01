998cad69374ab68ed1eac55fd8ef00a501938fa9
MernArtisan <mernartisan@gmail.com>
2025-07-01 23:39:28 +0500

    issue

--- routes/admin.php
+++ routes/admin.php
@@ -39,9 +39,8 @@ Route::prefix('admin')->name('admin.')->group(function () {
     Route::resource('/content', App\Http\Controllers\admin\ContentController::class);
     Route::resource('/testimonials', \App\Http\Controllers\admin\TestimoniController::class);
     Route::resource('/teams', \App\Http\Controllers\admin\TeamController::class);
-<<<<<<< HEAD
+
     Route::resource('/corporate-sponsors', \App\Http\Controllers\admin\CorprateSponserController::class);
-=======
     Route::get('/general-details', [App\Http\Controllers\admin\GeneralController::class, 'general'])->name('general.details');
     Route::get('/edit-general-details', [App\Http\Controllers\admin\GeneralController::class, 'edit'])->name('edit.general.details');
     Route::post('/update-general-details', [App\Http\Controllers\admin\GeneralController::class, 'update'])->name('update.general.details');
@@ -51,6 +50,5 @@ Route::prefix('admin')->name('admin.')->group(function () {
 
     Route::get('/inquiries', [App\Http\Controllers\admin\InquiryController::class, 'index'])->name('inquiries.index');
 
->>>>>>> origin/moiz
   });
 });
